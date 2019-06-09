de7609926276fc15221ba9bdcdeb6749c0dd6bb5
Anceag <maksimkorokin1@gmail.com>
2019-06-09 15:01:51 +0300

    Init

---
+++ StocksMobile/StocksMobile/Models/Item.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace StocksMobile.Models
+{
+    public class Item
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public string Description { get; set; }
+    }
+}
